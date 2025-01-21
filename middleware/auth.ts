d17f5500741fc9a978e7338c28a4968bd24d5347
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2025-01-21 10:59:23 -0500

    Comment middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const config = useRuntimeConfig();
-
-    // Vérifiez la configuration ou effectuez des actions spécifiques
+    
     if (!config.public.RECAPTCHA_SITE_KEY) {
         console.warn('La clé siteKey reCAPTCHA est manquante dans la configuration.');
     }
 
-    // Exemple de logique basée sur les routes
     if (to.name === 'protected-route') {
         console.log('Vous naviguez vers une route protégée.');
-        // Ajoutez votre logique ici, comme une vérification d'utilisateur ou une redirection
     }
 });
